4af259dffae4ac8cd2ddcf69a5ab69516ae724ae
Syed-Urwah <syedurwahdev@gmail.com>
2023-08-24 17:29:49 +0500

    24/8/23

--- client/src/pages/Admin/products/AddProduct.jsx
+++ client/src/pages/Admin/products/AddProduct.jsx
@@ -14,7 +14,7 @@ export default function AddProduct() {
 
                     <div id="form-container" className='w-full mt-7 pt-20'>
                         <form action="" className='flex gap-4'>
-                            <div id="left" className='w-[500px] flex flex-col gap-4'>
+                            <div id="left" className='w-full flex flex-col gap-4 outline outline-red-700'>
 
                                 <div id='first-row' className="flex flex-col gap-2">
                                     <label htmlFor="product-name" className=' text-gray-800'>Product Name</label>
@@ -49,27 +49,16 @@ export default function AddProduct() {
 
                             </div>
 
-                            <div id="right" className='w-fit'>
+                            <div id="right" className='w-full outline outline-green-700'>
                                 <div id='first-row' className="flex flex-col gap-2">
                                     <p className=' text-gray-800'>Product Image</p>
 
-                                    <div id="images" className='flex flex-wrap min-w-fit gap-2'>
+                                    <div id="images" className='flex flex-row-reverse flex-wrap min-w-fit gap-2'>
 
-                                        <div class="flex items-center justify-center w-60 ">
-                                            <label for="product-image" class="flex flex-col px-10 items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                                                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                                    </svg>
-                                                    <p class="mb-2 text-sm text-center text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                                                    <p class="text-xs text-center text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                                </div>
-                                                <input id="product-image" type="file" class="hidden" />
-                                            </label>
-                                        </div>
+                                      
 
 
-                                        <div id="right-images" className='flex flex-wrap gap-2'>
+                                        <div id="right-images" className='flex gap-2 '>
 
                                             <div class="flex items-center justify-center w-60 ">
                                                 <label for="product-image" class="flex flex-col px-10 items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
@@ -113,7 +102,18 @@ export default function AddProduct() {
                                             </div>
                                         </div>
 
-
+                                        <div class="flex items-center justify-center w-60 ">
+                                            <label for="product-image" class="flex flex-col px-10 items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                                    </svg>
+                                                    <p class="mb-2 text-sm text-center text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                                                    <p class="text-xs text-center text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                                </div>
+                                                <input id="product-image" type="file" class="hidden" />
+                                            </label>
+                                        </div>
 
                                     </div>
 
